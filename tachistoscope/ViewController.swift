363aab3dba2ae1818ffdeded49ac4c6c51547ece
小泉心之介 <shin200020529@gmail.com>
2016-06-03 20:01:11 +0900

    Initial Commit

---
+++ tachistoscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tachistoscope
+//
+//  Created by 小泉心之介 on 2016/06/03.
+//  Copyright © 2016年 小泉心之介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tachistoscopeTests/tachistoscopeTests.swift
@@ -0,0 +1,36 @@
+//
+//  tachistoscopeTests.swift
+//  tachistoscopeTests
+//
+//  Created by 小泉心之介 on 2016/06/03.
+//  Copyright © 2016年 小泉心之介. All rights reserved.
+//
+
+import XCTest
+@testable import tachistoscope
+
+class tachistoscopeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
