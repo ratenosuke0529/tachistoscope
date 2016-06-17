7ce393feff4c3d02dcd67ffbd89cd98d2bc02dc7
小泉心之介 <ratenosuke0529@gmail.com>
2016-06-17 20:18:07 +0900

    first commit

--- tachistoscope/ViewController.swift
+++ tachistoscope/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var  teamMemberLabel: UILabel!
+    
+    let teamMemberArray: [String] = ["へーわ"," ","そーり"," ","のすけ"," ","タカキ"," ","シュン"," ","はしお"," "]
+    
+    var index: Int = 0
+    
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func change(){
+       NSTimer.scheduledTimerWithTimeInterval(1,
+                    target: self,
+                    selector: #selector(self.countUp),
+                    userInfo: nil,
+                    repeats: true )
+                    
+        }
+    
+    func countUp () {
+        //それぞれのラベルに配列の要素を追加する
+        
+        teamMemberLabel.text = teamMemberArray[index]
+        
+        
+        
+        //indexの値を+1する
+        
+        
+        
+        index = index + 1
+        
+        
+        
+        //indexの値が5より多くなったら0に戻す
+        
+        if index > 5{
+            
+            index = 0
+            
+
+        
+    }
+    
+    }
 
 }
+
+
 
